2211e06a8a1b419706dd871a9fcb7121bbe20a42
ilumi <godjara1995@gmail.com>
2018-01-23 08:55:04 +0700

    New Project
    เพิ่ม Project SSPM

---
+++ ProjectSSPM/ProjectSSMP/Models/UserManagenent/DetailUserInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ProjectSSMP.Models.UserManagenent
+{
+    public class DetailUserInputModel
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string JobResponsible { get; set; }
+        public string UserEditBy { get; set; }
+        public DateTime? UserEditDate { get; set; }
+        public string GroupId { get; set; }
+        public string Status { get; set; }
+        public DateTime? UserCreateDate { get; set; }
+        public string UserCreateBy { get; set; }
+        public string GroupName { get; set; }
+
+    }
+}
---
+++ ProjectSSPM/ProjectSSMP/Models/UserManagenent/ShowUserInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ProjectSSMP.Models.UserManagenent
+{
+    public class ShowUserInputModel
+    {
+            public string UserId { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string JobResponsible { get; set; }
+            public string UserEditBy { get; set; }
+            public DateTime? UserEditDate { get; set; }
+            public string GroupId { get; set; }
+            public string Status { get; set; }
+
+    }
+}
